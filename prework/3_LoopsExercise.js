186972c9e275b91e96a03ee52d509494121a8580
serenaqchen <serenaqchen@gmail.com>
2021-07-01 14:31:04 -0700

    Finished loops, arrays, and object exercises

--- prework/3_LoopsExercise.js
+++ prework/3_LoopsExercise.js
@@ -11,36 +11,117 @@
 
 // Exercise 1. Write a 'while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
+let count = 1
+
+while (count <= 5) {
+  console.log(count);
+  count ++;
+}
 
 // Exercise 2. Write a 'do while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
 
+let count = 1
+do{
+  console.log(count);
+  count ++;
+}
+while (count <= 5)
+
 // Exercise 3. Write a 'for' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
+
+for (let num = 1; num <=5; num++){
+  console.log(num);
+}
 
 // Exercise 4. Now we want a loop that prints the integers
 // counting DOWN from 10 to 1 (inclusive). Write three loops that do this, to practice
 // writing loops in all 3 ways -- as a 'while', 'do while', and 'for' loop.
 
+let count1 = 10
+do{
+  console.log(count1);
+  count1 -- ;
+}
+while (count1 > 0);
+
+let count2 = 10
+
+while (count2 > 0) {
+  console.log(count2);
+  count2 --;
+};
+
+for (let num = 10; num > 0; num--){
+  console.log(num);
+};
+
 // Exercise 5. Write a loop that prints the integers from 7 to 27. Write this
 // loop in all 3 ways -- as a 'while', 'do while', and 'for' loop.
+
+let count1 = 7
+do{
+  console.log(count1);
+  count1 ++ ;
+}
+while (count1 <= 27);
+
+let count2 = 7
+
+while (count2 <= 27) {
+  console.log(count2);
+  count2 ++;
+};
+
+for (let num = 7; num <= 27; num++){
+  console.log(num);
+};
 
 // Exercise 6. Write a loop that prints numbers between 0 and 100, counting by tens.
 // I.e. it will print 10, then 20, then 30, etc.
 // Write this loop in all 3 ways -- as a 'while', 'do while', and 'for' loop.
 
+let count1 = 10
+do{
+  console.log(count1);
+  count1 += 10 ;
+}
+while (count1 <= 100);
+
+let count2 = 10
+
+while (count2 <= 100) {
+  console.log(count2);
+  count2 += 10;
+};
+
+for (let num = 10; num <= 100; num+= 10){
+  console.log(num);
+};
 // Exercise 7. Add a comment as to why the following loop is an infinite loop (will
 // run without ever stopping). Then fix the loop so that it stops when
 // counterFour is equal to -100.
 
+// The while loop is infinite because counterFour starts off as 1 which is less that 2.
+// Therefore, the loop will run and subtract from counterFour. 
+// Since it is always subtracting from counterFour, the condition of counterFour < 2 will always be met 
+// and the while loop will always run. Hence, you'll have an infinite loop. 
+
 let counterFour = 1;
-while (counterFour < 2) {
+while (counterFour > -100) {
   console.log("HELP ME!");
   counterFour--;
 }
 
 // Exercise 8. Make a variable that contains your favorite integer. Write a loop
 // (your choice which type) that prints the integers from 0 to that number.
+
+let favNum = 8;
+
+for (let num = 0; num <= favNum; num ++){
+  console.log(num);
+}
 
 // Exercise 9. Make a variable that contains your favorite integer (this time make sure it's
 // less than 100). Write a loop (your choice which type) that prints the integers from 0 to 100.
@@ -53,11 +134,29 @@ while (counterFour < 2) {
 // ...
 // (Hint - use an if statement in your loop)
 
+let favNum = 8;
+
+for (let num = 0; num <= 100; num ++){
+  if (num === favNum){
+    console.log(num + " my favorite number!");
+  } else {
+    console.log(num + " not my favorite number");
+  }
+}
+
+
 // Exercise 10. In some of the exercises above, we had you write all 3 types of loops, for practice.
 // But in real life, how would you decide which type of loop to use? You might not have an
 // exact answer, but spend a few minutes thinking about the different types of loops and/or
 // doing some research and write down your thoughts in a comment below:
 //
+
+// In general you should use a for loop when you know exactly how many times you want to run the loop.
+// You should use a while loop when you want the loop to break based on a certian condition
+// and you don't have a specific number of interations in mind. 
+// The do while and while loop are similar, but different in the sense that the do while checks the 
+// condition AFTER the loop is ran. Therefore the loop will always run at least once. 
+// The while loop checks condition BEFORE it may run the loop, so it may run the loop 0 or more times
 
 // Exercise 11. Now we'll practice using nested loops (a loop inside another loop)!
 // Update the nested loops below to so that for each value of outsideCounter,
@@ -80,10 +179,10 @@ inside 1
 *********************************** 
 */
 
-for (let outsideCounter = 0; outsideCounter <= 3 /*???*/; ) {
+for (let outsideCounter = 0; outsideCounter <= 3; outsideCounter++) {
   console.log("counting down from", outsideCounter);
-  for (let insideCounter = outsideCounter /*???*/ /*???*/; ; ) {
-    console.log("inside" /*???*/);
+  for (let insideCounter = outsideCounter; insideCounter > 0; insideCounter-- ) {
+    console.log("inside", insideCounter)
   }
   console.log("***********************************");
 }
--- prework/5_ArraysObjectsExercise.js
+++ prework/5_ArraysObjectsExercise.js
@@ -10,29 +10,37 @@
 // But make sure you understand why the correct answer is right.
 
 // Exercise 1. Make an empty array named animals
+let animals = [];
 
 // Exercise 2. Add the string "frog" to the array
+animals.push("frog");
 
 // Exercise 3. Add 4 more animals of your choice to the array. Try to add all of them in one line of code.
+animals.push("panda", "snake", "kangaroo", "dog");
 
 // Exercise 4. Update the first item in the array to be "gorilla"
+animals[0]="gorilla";
 
 // Exercise 5. Print the number of items in the array (use the array property that will give you this information)
+animals.length;
 
 // Exercise 6. Print the first item in the array
-
+animals[0];
 // Exercise 7. Print the last item in the array (as part of your answer, use the same array property you used in exercise 5 above)
-
+animal[animals.length-1];
 // Exercise 8. Remove the last item from the array
-
+animals.pop();
 // Exercise 9. Now make a new array named assortedThings that contains at least 3 strings and 3 numbers.
+let assortedThings = ["hello", "friends", "!", 1, 2, 3];
 
 // Exercise 10. Write a for loop that iterates through the assortedThings array and prints each index and item, such as:
 // Item #0 is potato
 // Item #1 is 4
 // Item #2 is Hello World
 // ...
-
+for (let index = 0; index <=assortedThings.length-1; index++){
+    console.log ("Item #" + index + " is " + assortedThings[index]);
+}
 // Exercise 11. Write a function that takes an array of numbers as a parameter. For each number in the array,
 // print the number and "BIG" if it's over 100, "small" if it's between 0 and 100, and "negative" if
 // it's less than 0. Example:
@@ -41,18 +49,37 @@
 // 200 BIG
 // 50 small
 //
+function assignDescription(array){
+    for (num of array){
+        if (num > 100){
+            console.log(num + " BIG");
+        } else if (num >=0 && num <= 100){
+            console.log(num + " small");
+        } else {
+            console.log(num + " negative");
+        }
+    }
+}
 // Then write some function calls you would use to test your function.
+assignDescription([-2, 200, 50])
 
 // Exercise 12. Make an object called me that contains the following keys: name, favoriteAnimal, favoriteNumber
 // The values of those keys can be whatever you choose.
+let me = {
+    name: "Serena",
+    favoriteAnimal: "Elephant",
+    favoriteNumber: "2"
+};
 
 // Exercise 13. Add another 2 keys and values to the object (you choose what they are)
-
+me["favoriteFood"] = "Noodles"
+me["favoriteMovie"] = "Soul"
 // Exercise 14. Update the favoriteAnimal value to something different
-
+me["favoriteAnimal"] = "Bunny"
 // Exercise 15. Print the value of favoriteAnimal.
 // Note: there are at least 2 ways to get the value of a key, try to write both.
-
+me.favoriteAnimal
+me["favoriteAnimal"]
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you?
 // Please answer in a comment below.
