5d8c9291bd054070c38da380314764318f0214e1
serenaqchen <serenaqchen@gmail.com>
2021-07-01 15:04:41 -0700

    finished week 3 functions exercises

--- prework/4_FunctionsExercise.js
+++ prework/4_FunctionsExercise.js
@@ -11,29 +11,55 @@
 
 // Exercise 1. Define a function called logGreeting() that prints the string “Hello!” (use console.log).
 // Underneath the function, write the line of code that runs the function.
-
+function logGreeting(){
+  console.log("Hello");
+}
+logGreeting();
 // Exercise 2. Define a function called getName() that *returns* a string that is your name.
 // Remember, this function should return the string -- not print it.
 // Then print your name in the console by passing getName() into the console.log() function.
-
+function getName(){
+  return "Serena";
+}
+console.log(getName());
 // Exercise 3. Call a function within a function -- Write a function called logGreeting2()
 // that prints a full sentence that contains your name: "Hello! My name is <name>."
 // logGreeting2 should call myName() to get your name.
 // Then print your greeting to the console by calling logGreeting2().
-
+function logGreeting2(){
+  console.log("Hello! My name is " + logGreeting()+ ".");
+}
+logGreeting2();
 // Exercise 4. Write a function that takes 3 parameters that are all numbers.
 // The function should return the sum of the 3 numbers.
 // Then write some function calls you would use to test your function.
-
+function sumAll(num1, num2, num3){
+  return num1+num2+num3;
+}
+sumAll(1,2,4);
 // Exercise 5. Let's say a museum gives a discount for children ages 14 or under, and seniors 65 or older.
 // Write a function that takes in a person's age and returns true if they should get a discount.
 // Otherwise it should return false.
 // Then write some function calls you would use to test your function.
-
+function discount(age){
+  if (age <= 14 || age >= 65){
+    return true;
+  }else{
+    false;
+  }
+}
+discount(2);
 // Exercise 6. Write a function that takes 2 parameters -- one number and one string.
 // The function should print the string the given number of times.
 // Then write some function calls you would use to test your function.
-
+function repeat(num, string){
+  let count = 1;
+  while (count <= num){
+    console.log(string);
+    count ++;
+  }
+}
+repeat(4, "Hello");
 // Exercise 7. Read the following code (don't run it yet)
 function mysteryFunction1(p1) {
   return p1 * 2;
@@ -45,11 +71,14 @@ console.log("The value of z is " + z);
 
 // Without running the code, write down in a comment:
 // 1. What mysteryFunction1 does
+// mysteryFunction takes in a number and doubles it
 // 2. What prints out for the value of y
+// 4
 // 3. What prints out for the value of z
+// 8
 // Now run the code and see if you're correct.
 // Were you correct? If not, what did you learn?
-
+// Yes.
 // Exercise 8. Read the following code (don't run it yet)
 function mysteryFunction2(p1, p2) {
   const x = p1 - p2;
@@ -62,11 +91,14 @@ console.log("The value of b is " + b);
 
 // Without running the code, write down in a comment:
 // 1. What mysteryFunction2 does
+// This function takes the difference of two numbers 
 // 2. What prints out for the value of a
+// 6
 // 3. What prints out for the value of b
+// 5
 // Now run the code and see if you're correct.
 // Were you correct? If not, what did you learn?
-
+// Yes.
 // Exercise 9. This exercise is to practice reading the documentation for functions.
 // String.split() is a function in the JavaScript standard library that you can use in your code.
 // Read about the split() function here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
@@ -75,12 +107,12 @@ console.log("The value of b is " + b);
 // Here's a string I made representing my grocery list.
 // Using split() and groceryList, make an array of my grocery list items
 const groceryList = "eggs,carrots,orange juice";
-
+groceryList.split(",");
 // Here's a string I made representing my morning schedule.
 // Using split() and mySchedule, make an array of the *first 2* things I do in the morning
 // There are multiple ways to do this, but try doing it using only the split() function.
 const mySchedule = "wake up--->brush teeth--->eat breakfast--->go to work";
-
+mySchedule.split("--->")
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you?
 // Please answer in a comment below.
